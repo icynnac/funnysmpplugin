cbe3fe5150f2936e46af528dfef9bb4ccfb45407
icynnac <icynnac@gmail.com>
2022-03-06 15:08:12 -0500

    New Commands

--- src/main/java/me/rllol/smp/commands/chatcommands.java
+++ src/main/java/me/rllol/smp/commands/chatcommands.java
@@ -1,8 +1,10 @@
 package me.rllol.smp.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,6 +13,21 @@ import java.util.List;
 public class chatcommands implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            switch (label.toLowerCase()) {
+                case "heart":
+                    p.chat("§c❤");
+                    break;
+                case "boop":
+                    if (args.length > 0) {
+                        Player t = Bukkit.getPlayer(args[0]);
+                        p.sendMessage("§6SMP §8>> §d§lYou Booped " + t.getName() + "!");
+                        t.sendMessage("§6SMP §8>> §d§l" + p.getName() + " has Booped you!");
+                    }
+                    break;
+            }
+        }
         return false;
     }
 
