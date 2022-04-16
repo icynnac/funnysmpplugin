2f3fc071ebd54a7602ded31ad5b059b0cc89246e
icynnac <icynnac@gmail.com>
2022-04-15 20:30:04 -0400

    /ping sounds + no console restrictions

--- src/main/java/me/rllol/smp/commands/ping.java
+++ src/main/java/me/rllol/smp/commands/ping.java
@@ -1,6 +1,8 @@
 package me.rllol.smp.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,15 +11,18 @@ import org.bukkit.entity.Player;
 public class ping implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            if (args.length > 0) {
-                Player t = Bukkit.getPlayer(args[0]);
-                if (t != null) {
-                    p.sendMessage("§6SMP §8>> §b"+ t.getName() + "'s ping is" + t.getPing() + "ms");
-                }
-            } else p.sendMessage("§6SMP §8>> §bYour ping is " + p.getPing() + "ms");
-        } else sender.sendMessage("no");
+        Player p = (Player) sender;
+        Location loc = p.getLocation();
+        if (args.length > 0) {
+            Player t = Bukkit.getPlayer(args[0]);
+            if (t != null) {
+                p.sendMessage("§6SMP §8>> §b"+ t.getName() + "'s ping is " + t.getPing() + "ms");
+                p.playSound(loc, Sound.ENTITY_VILLAGER_AMBIENT, 1,1);
+            }
+        } else {
+            p.sendMessage("§6SMP §8>> §bYour ping is " + p.getPing() + "ms");
+            p.playSound(loc, Sound.ENTITY_VILLAGER_AMBIENT, 1,1);
+        }
         return false;
     }
 }
